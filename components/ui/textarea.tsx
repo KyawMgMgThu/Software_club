8ea5cbff94527d476345a5061c9af49ed736f49f
kyawmgmgthu <mthu35997@gmail.com>
2024-11-22 18:40:32 +0630

    deploy error fix2

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -1,10 +1,8 @@
-"use client";
-
 import * as React from "react";
 import { cn } from "@/lib/utils";
 
-export interface TextareaProps
-  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface TextareaProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
 
 const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
   ({ className, ...props }, ref) => {
